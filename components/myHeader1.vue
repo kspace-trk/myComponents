37152959501f79b52286d909a6f499990530df88
中尾圭吾 <knakao@nakaokeigonoiMac.local>
2020-06-18 14:08:38 +0900

    a

---
+++ components/myHeader1.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+    <div class="header-container">
+      <div class="header-left">
+        <p>ロゴ</p>
+      </div>
+      <div class="header-right">
+        <div class="header-right-contents">
+          <p>about</p>
+        </div>
+        <div class="header-right-contents">
+          <p>Works</p>
+        </div>
+        <div class="header-right-contents">
+          <p>Access</p>
+        </div>
+        <div class="header-right-contents">
+          <p>Contact</p>
+        </div>
+      </div>
+    </div>
+
+    <header>
+      <div id="nav-drawer">
+        <input id="nav-input" type="checkbox" class="nav-unshown" />
+        <label id="nav-open" for="nav-input">
+          <span></span>
+        </label>
+        <label class="nav-unshown" id="nav-close" for="nav-input"></label>
+        <div id="nav-content">ここに中身を入れる</div>
+      </div>
+    </header>
+  </div>
+</template>
+    
+<style scoped>
+.header-container {
+  width: 100%;
+  height: 75px;
+  background-color: #404040;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+@media screen and (max-width: 500px) {
+  .header-container {
+    display: none;
+  }
+}
+.header-container p {
+  color: #e7e7e7;
+}
+.header-left {
+  margin-left: 20px;
+}
+.header-right {
+  display: flex;
+}
+.header-right-contents {
+  margin-right: 30px;
+}
+
+header {
+  padding: 10px;
+}
+
+#nav-drawer {
+  position: relative;
+}
+@media screen and (min-width: 501px) {
+  #nav-drawer {
+    display: none;
+  }
+}
+
+/*チェックボックス等は非表示に*/
+.nav-unshown {
+  display: none;
+}
+
+/*アイコンのスペース*/
+#nav-open {
+  display: inline-block;
+  width: 30px;
+  height: 22px;
+  vertical-align: middle;
+}
+
+/*ハンバーガーアイコンをCSSだけで表現*/
+#nav-open span,
+#nav-open span:before,
+#nav-open span:after {
+  position: absolute;
+  height: 3px; /*線の太さ*/
+  width: 25px; /*長さ*/
+  right: 0px;
+  border-radius: 3px;
+  background: #555;
+  display: block;
+  content: "";
+  cursor: pointer;
+}
+#nav-open span:before {
+  bottom: -8px;
+}
+#nav-open span:after {
+  bottom: -16px;
+}
+
+/*閉じる用の薄黒カバー*/
+#nav-close {
+  display: none; /*はじめは隠しておく*/
+  position: fixed;
+  z-index: 99;
+  top: 0; /*全体に広がるように*/
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: black;
+  opacity: 0;
+  transition: 0.3s ease-in-out;
+}
+
+/*中身*/
+#nav-content {
+  overflow: auto;
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9999; /*最前面に*/
+  width: 90%; /*右側に隙間を作る（閉じるカバーを表示）*/
+  max-width: 330px; /*最大幅（調整してください）*/
+  height: 100%;
+  background: #fff; /*背景色*/
+  transition: 0.3s ease-in-out; /*滑らかに表示*/
+  -webkit-transform: translateX(105%);
+  transform: translateX(105%); /*左に隠しておく*/
+}
+
+/*チェックが入ったらもろもろ表示*/
+#nav-input:checked ~ #nav-close {
+  display: block; /*カバーを表示*/
+  opacity: 0.5;
+}
+
+#nav-input:checked ~ #nav-content {
+  -webkit-transform: translateX(0%);
+  transform: translateX(0%); /*中身を表示（右へスライド）*/
+  box-shadow: 6px 0 25px rgba(0, 0, 0, 0.15);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <div class="container">
+    <myHeader1 />
+  </div>
+</div>
+</template>
+
+<script>
+import myHeader1 from '~/components/myHeader1.vue'
+
+export default {
+  components: {
+    myHeader1
+  }
+}
+</script>
+
+<style scoped>
+</style>
